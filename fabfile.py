e1206ee2097947f14318199cfd085c2753700591
Ryan Showalter <ryan@perkville.com>
2012-06-21 13:50:18 -0700

    Add apache project generation

--- fabfile.py
+++ fabfile.py
@@ -12,6 +12,90 @@ env.roledefs = {
 
 
 @task
+def create_apache_project(project, domain, py_version='2.7'):
+
+    # Check to see if this domain already exists.. abort if it does
+    if exists("/opt/webapps/{domain}".format(domain=domain)):
+        print red("This project already exists.")
+        return
+
+    # Check to see if Apache is already broken and warn the user about it
+    apache_broken = False
+    with settings(warn_only=True):
+        apache_status = sudo("apache2ctl -t")
+        if not apache_status.succeeded:
+            apache_broken = True
+            response = prompt(red("Apache is currently BROKEN!  Are you SURE you want to continue?"), default="n")
+            if response.lower() == 'n':
+                return
+
+    # Create the group for this project if it does not exist
+    sudo("groupadd -f {project}".format(project=project))
+
+    # Add the apache user to this project's group so it can read what it needs to read
+    sudo("usermod -a -G {project} www-data".format(project=project))
+
+    # Create the directory, chmod it
+    with cd("/opt/webapps"):
+        sudo("mkdir -p {domain}/static".format(domain=domain))
+
+    with cd("/opt/webapps/{domain}".format(domain=domain)):
+        sudo("virtualenv --distribute --no-site-packages --python=python{py_version} env".format(py_version=py_version))
+        sudo("chmod -R 2770 ./")
+
+    cxt = {
+        'py_version': py_version,
+        'project': project,
+        'domain': domain,
+        'ip_address': run("hostname -i"),}
+
+    upload_template(
+        "myvirtualdjango.py",
+        "/opt/webapps/{domain}/env/myvirtualdjango.py".format(domain=domain),
+        context=cxt,
+        use_jinja=True,
+        template_dir="templates",
+        use_sudo=True,
+        mode=770)
+
+    # Upload the apache project template
+    upload_template(
+        "project-apache.conf",
+        "/opt/webapps/{domain}/{domain}.apache".format(domain=domain),
+        context=cxt,
+        use_jinja=True,
+        template_dir="templates",
+        use_sudo=True,
+        mode=774)
+
+    # Upload the README template
+    upload_template(
+        "README",
+        "/opt/webapps/{domain}/README".format(domain=domain),
+        context=cxt,
+        use_jinja=True,
+        template_dir="templates",
+        use_sudo=True,
+        mode=770)
+
+    # Chown everything in the directory to root:{project}
+    sudo("chown -R root:{project} /opt/webapps/{domain}".format(project=project, domain=domain))
+
+    # Create a sym-link from the apache config to sites-enabled
+    sudo("ln -sf /opt/webapps/{domain}/{domain}.apache /etc/apache2/sites-enabled/{domain}".format(domain=domain))
+
+    # Check apache to make sure the config we just sym-linked is valid.. if it's not, remove the link
+    with settings(warn_only=True):
+        apache_status = sudo("apache2ctl -t")
+        if not apache_status.succeeded and not apache_broken:
+            print red("Your Apache config file is BAD!  I'm going to remove the sym-link so we don't break the server.")
+            sudo("rm /etc/apache2/sites-enabled/{domain}".format(domain=domain))
+
+    # Reload apache so the config changes take effect
+    sudo("/etc/init.d/apache2 reload")
+
+
+@task
 def create_nginx_project(project, domain, py_version='2.7'):
 
     # Check to see if this domain already exists.. abort if it does
@@ -53,7 +137,7 @@ def create_nginx_project(project, domain, py_version='2.7'):
     # Upload the uwsgi config template
     upload_template(
         "myvirtualdjango.py",
-        "/opt/webapps/{domain}/myvirtualdjango.py".format(domain=domain),
+        "/opt/webapps/{domain}/env/myvirtualdjango.py".format(domain=domain),
         context=cxt,
         use_jinja=True,
         template_dir="templates",
